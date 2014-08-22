e41ac8d8bbdbbcf641de98d50168f6c314886d3e
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-08-22 07:53:02 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
+app.use(allowCrossDomain);
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + 'index.html');
+//   res.end();
+// });
+
+app.get('/', function (req, res){
+  console.log('hello');
+  res.send('hello my friend');
+});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
+
+console.log("Listening on port " + port);
